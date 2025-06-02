0e972c2fd41d5143e5a60a60f7c34745d0a592db
Maydic489 <74528891+Maydic489@users.noreply.github.com>
2025-06-02 15:06:54 +0700

    rollback

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-blog",
   themeConfig: "./theme.config.js",
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 });
 
-module.exports = withNextra({
+/** @type {import('next').NextConfig} */
+const nextConfig = withNextra({
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  // Add this if your repo isn't at root of your GitHub account
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name/',
 });
+
+module.exports = nextConfig;
