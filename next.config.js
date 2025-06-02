81983b63f802c9df07293801c9920a73efe54a32
Maydic489 <74528891+Maydic489@users.noreply.github.com>
2025-06-02 15:02:22 +0700

    use nextra

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-blog",
   themeConfig: "./theme.config.js",
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withNextra({
+module.exports = withNextra({
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Add this if your repo isn't at root of your GitHub account
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
 });
-
-module.exports = nextConfig;
